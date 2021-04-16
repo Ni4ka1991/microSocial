fef5d7ddf9e546a20c758d95bc134ba1e0dadf94
Ni4ka <astahova_nica@mail.ru>
2021-04-16 12:00:41 +0300

    Add file app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env Python3
 
-# MAIN MODULE
+# MAIN MODULE app.py
+
+ ## imports ######
+
 import math
 
 from data import *
@@ -8,9 +11,11 @@ from data import *
 from os import system
 from random import randint
 
+ ## ###############
 
 pages = math.ceil( len( users )/USERS_PG )
 
+ ##   VIEW  #######################
 
 def userList( pg ):
  
@@ -18,13 +23,19 @@ def userList( pg ):
  
  print( f"THE LIST OF USERS IS {pages} PAGES.\n" )
  
+  ###  print a specified interval #######
+
  for i in range( USERS_PG * ( pg - 1 ), min( USERS_PG * pg, len( users ))): 
   name   = users[i]["name"]
   city   = users[i]["address"]["city"]
   status = "(O)" if users[i]["online"] else "(-)"
   print( f"{( i + 1 ):2} - {name:25} ( {city:15} ) {status}" )
 
+  ###  print a specified interval #######
+
  print( "-" * 54 )
+
+  ### print a pages number ##############
 
  for k in range( 1, pages + 1 ):
   if( k == pg ):
@@ -34,21 +45,26 @@ def userList( pg ):
   
  print()
 
- ####### PRINT USER LIST #########
+  ### print a pages number ##############
 
-anchor = 1
+ ##  VIEW  #############################
 
-userList( anchor ) 
+ ##  LOGIC  ############################
+
+anchor = 1 # which page will be show first
+
+userList( anchor ) # print the first page userList( 1 )
 
 while True:
 
  navigator = input( "\nEnter p to change page number or < / >  to go to the prev / next pages. Enter x to exit. >>>  " )
  print()
+ 
  if( navigator == "p" ):
    invite = int( input( f"Select a page from the range 1 to {pages}  >>>  " ))
   
-   if( anchor <= invite <= pages ):   
-     userList( invite )
+   if( anchor <= invite <= pages ): # if entered number in range 1( anchor ) - 5 ( all pages) 
+     userList( invite )             # print list
 
    else:
      print( "Such page is not in the list" )
@@ -69,23 +85,12 @@ while True:
    else:
     print( f"You are already  on the last < {pages} > page." )
     input( "hit ENTER to continue" )
- elif( navigator == "x"):
+ 
+ elif( navigator == "x" ):
   print( "By, by!" )
   break
+
  else:
-  print( "Be careful!" )
+  print( "Be careful!" ) #lazy check
  
-
-
-
-
-
-
-
-
-
-
-
- 
-# ####### PRINT USER LIST #########
-
+ ## LOGIC #####################################
